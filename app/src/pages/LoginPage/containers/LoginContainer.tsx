3a6e0ccaf73bc453994ef45c945a9ac1cb8260d9
adryian <adryian.siarheyeu@itechart-group.com>
2021-08-19 13:30:10 +0300

    eslint added

--- app/src/pages/LoginPage/containers/LoginContainer.tsx
+++ app/src/pages/LoginPage/containers/LoginContainer.tsx
@@ -1,18 +1,18 @@
-import {FC, useCallback, useLayoutEffect} from "react";
-import {useForm, useTypedSelector} from "../../../hooks";
-import LoginForm from "../components/LoginForm/LoginForm";
-import {useDispatch} from "react-redux";
-import {LOGIN_REQUEST} from "../actions";
-import {useHistory} from "react-router-dom";
+import { FC, useCallback, useLayoutEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { useForm, useTypedSelector } from '../../../hooks';
+import LoginForm from '../components/LoginForm/LoginForm';
+import { LOGIN_REQUEST } from '../actions';
 
-import {SignInDto} from "../dto/login.dtos";
-import {ROUTES} from "../../../router/routeNames";
+import { SignInDto } from '../dto/login.dtos';
+import { ROUTES } from '../../../router/routeNames';
 
 const LoginContainer: FC = () => {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const { formValues, handleFormFieldChange } = useForm<SignInDto>({ email: "", password: "" });
+  const { formValues, handleFormFieldChange } = useForm<SignInDto>({ email: '', password: '' });
 
   const { isLoading, errors, isAuth } = useTypedSelector((state) => state.auth);
 
